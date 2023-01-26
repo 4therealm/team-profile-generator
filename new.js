92505afce992e11fefa1ca922cd995f1b59514d4
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-25 22:46:00 -0600

    finished core functions

--- new.js
+++ new.js
@@ -1,145 +1,127 @@
-
-const inquirer = require('inquirer')
-let applicant =[]
-const team = []
+const inquirer = require ( 'inquirer' );
+const team = [];
 const basicInfo = [
-  {
-    type: 'input',
-    name:'name',
-    message: 'what is their name?',
-  },
-  {
-    type: 'input',
-    name:'Id',
-    message: 'what is their ID?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is their email?'
-  },
-]
-const engineerQuestions = [
-  {
-    type: 'input',
-    message: 'What is their Github name?',
-    name: 'github'
-  }
-]
-const internQuestions = [
-  {
-    type:'input',
-    message:'What school did they go to?',
-    name:'school',
-  }
-]
-class Employee{
-  constructor(name, id, email){
+  { type: 'input', name: 'name', message: 'what is their name?' },
+  { type: 'input', name: 'Id', message: 'what is their ID?' },
+  { type: 'input', name: 'email', message: 'What is their email?' },
+];
+
+class Employee {
+  constructor ( name, id, email ) {
     this.name = name;
     this.id = id;
     this.email = email;
   }
-getName(){
-  console.group(this.name)
-  return this.name
+  getName () {
+    console.log ( this.name );
+    return this.name;
+  }
+  getId () {
+    console.log ( this.id );
+    return this.id;
+  }
+  getEmail () {
+    console.log ( this.email );
+    return this.email;
+  }
 }
-getId(){
-  console.group(this.id)
-  return this.id
+class Manager extends Employee {
+  constructor ( name, id, email, officeNumber ) {
+    super ( name, id, email, );
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber () {
+    return `Office Number: ${
+      this.officeNumber
+    }`;
+  }
 }
-getEmail(){
-  console.group(this.email)
-  return this.email
+class Engineer extends Employee {
+  constructor ( name, id, email, github ) {
+    super ( name, id, email );
+    this.github = github;
+  }
+  getGithub () {
+    return this.github;
+  }
 }
-}
-class Manager extends Employee{
-    constructor(name, id, email, officeNumber){
-      super(name, id, email,)
-      this.officeNumber = officeNumber
-      }
-      getOfficeNumber(){return `Office Number: ${this.officeNumber}`}    
-}
-class Engineer extends Employee{
-    constructor(name, id, email, github){
-      super(name, id, email)
-      this.github = github
-    }
-    getGithub(){return this.github}
-}
-class Intern extends Employee{
-    constructor(name, id, email, school){
-      super(name, id, email)
-      this.school = school;
-    }
-    getSchool(){return `School: ${this.school}`} 
-}
-// applicant.push(answers.name);
-      // applicant.push(answers.id);
-      // applicant.push(answers.email);
-start()
-function start(){
-  inquirer
-  .prompt({
-    type:'list',
-    name: 'addEmployee',
-    message:'Would you like to add an employee?',
-    choices: ['yes', 'no']
-  })
-  .then((data) => {data ? getBasicInfo() : exit()});
-}
-function getBasicInfo() {
-    inquirer
-    .prompt({
-      type: 'list',
-      name: 'role',
-      message: 'What position would you like to fill?',
-      choices: ['Manager', 'Engineer', 'Intern'],
-    })
-    .then((role)=>{
-       let position = role.role
-      switch(position) {
-        case 'Manager':
-         getManagerInfo()
-        //  start()
-        console.log(team)
-         break;
-        case 'Engineer':
-          applicant = getEngineerInfo()
-         console.log(applicant)
-         break;
-        case 'Intern':
-          applicant = getInternInfo()
-         console.log(applicant)
-         break;
-     }
-    })
-}
+class Intern extends Employee {
+  constructor ( name, id, email, school ) {
+    super ( name, id, email );
+    this.school = school;
+  }
+  getSchool () {
+    return `School: ${
+      this.school
+    }`;
+  }
+} 
 
-function getManagerInfo(){
-  basicInfo.push({type: 'input', message: 'What is their office number?', name:'officeNumber'})
-    inquirer
-    .prompt(basicInfo)
-    .then(data =>{
-      const newEmployee = new Manager(data.name, data.id, data.email, data.officeNumber)
-      team.push(newEmployee)
-    })
+start ();
+
+function start () {
+  inquirer
+  .prompt ( { 
+    type: 'list',
+    name: 'addEmployee', 
+    message: 'Would you like to add an employee?', 
+    choices: [ 'yes', 'no' ] } )
+  .then ( ( data ) => {data ? getPosition () : exit ();});
 }
-function getEngineerInfo(employeeObj){
-    inquirer
-    .prompt(engineerQuestions)
-    .then(data =>{
-      let gH = data.github
-      console.log(data)
-      console.log(employeeObj)
-      return `${gH}`
-    })
+function getPosition () {
+  inquirer
+  .prompt ( {
+    type: 'list', 
+    name: 'role', 
+    message: 'What position would you like to fill?', 
+    choices: [ 'Manager', 'Engineer', 'Intern' ] } )
+  .then ( ( choice ) => {
+    switch ( choice.role ) {
+      case 'Manager': hireManager ();
+        console.log ( team.length );
+        break;
+      case 'Engineer': hireEngineer ();
+        console.log ( team.length );
+        break;
+      case 'Intern': hireIntern ();
+        console.log ( team.length );
+        break;
+    }
+  } );
 }
-function getInternInfo(employeeObj){
-    inquirer
-    .prompt(internQuestions)
-    .then(data =>{
-     let skewl = data.school
-     console.log(data)
-     return `${skewl}`
-    })
+function hireManager () {
+  basicInfo.push ( { 
+    type: 'input', 
+    message: 'What is their office number?', 
+    name: 'officeNumber' } );
+  inquirer
+    .prompt ( basicInfo )
+    .then ( data => {
+      const newManager = new Manager ( data.name, data.id, data.email, data.officeNumber );
+      team.push ( newManager );
+  } );
+}
+function hireEngineer () {
+  basicInfo.push ( { 
+    type: 'input', 
+    message: 'What is their Github name?', 
+    name: 'github' } );
+  inquirer
+    .prompt ( basicInfo )
+    .then ( data => {
+      const newEngineer = new Engineer ( data.name, data.id, data.email, data.github );
+      team.push ( newEngineer );
+  } );
+}
+function hireIntern () {
+  basicInfo.push ( { 
+    type: 'input', 
+    message: 'What school did they go to?', 
+    name: 'school' } );
+  inquirer
+    .prompt ( basicInfo )
+    .then ( data => {
+      const newIntern = new Intern ( data.name, data.id, data.email, data.school );
+      team.push ( newIntern );
+  } );
 }
